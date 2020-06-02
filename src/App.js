22891cb367b0ca2a053f0ed01827fbc74b9f16d8
HassamSaeed <hassamsaeed@users.noreply.github.com>
2020-06-02 23:04:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hassam
+          Learn React
         </a>
       </header>
     </div>
